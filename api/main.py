9d07d1a4089f5301e0a2de99a52e92681725bfd3
LFlops <glide_nether0x@icloud.com>
2025-08-16 20:29:18 +0800

    fix github action

--- api/main.py
+++ api/main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from api!")
+# main.py
+from fastapi import FastAPI
 
+# 创建 FastAPI 实例
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+# 定义一个根路由
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+# 带路径参数的路由
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
